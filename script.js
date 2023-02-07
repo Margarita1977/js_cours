c2924ccca08b7550a97323cc52f77ac34a5f0a95
Margarita1977 <rita.platonova.02@mail.ru>
2023-02-07 12:59:39 +0300

    lesson2

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const title = 1;
-const screens = 1;
-const screenPrice = 1;
-const rollback = 4;
-const fullPrice = 1;
-const adaptive = 1;
+const title = "Проект js";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = Math.random();
+const rollback = Math.ceil(Math.random()*100);
+const fullPrice = "400700" ;
+const adaptive = true || false ;
 
 alert ('Hello');
 
+console.log(title);
+console.log(fullPrice);
 console.log(adaptive);
-
+console.log(screens.length);
+console.log(screens.toLowerCase());
+console.log(screens.split(" "));
+console.log("Стоимость разработки сайта", fullPrice, "рублей/ долларов/гривен/юани");
+console.log(fullPrice * (rollback/100));
